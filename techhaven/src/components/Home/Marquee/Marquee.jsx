fff7a8e918a0a3a789e9bcd56071fcbaab24b98a
krish189 <donkrish290@gmail.com>
2024-10-05 15:58:14 +0530

    Marquee changes

--- techhaven/src/components/Home/Marquee/Marquee.jsx
+++ techhaven/src/components/Home/Marquee/Marquee.jsx
@@ -9,6 +9,10 @@ function Marquee() {
             <p><img src={dot} alt='dot' className='doticon'/> TechHaven: Your Reliable Tech Partner </p>
             <p><img src={dot} alt='dot' className='doticon'/> Join the TechHaven Family! Your Satisfaction is Our Top Priority! </p>
             <p><img src={dot} alt='dot' className='doticon'/> Experience Unmatched Quality with Every Product at TechHaven! </p>
+            <p><img src={dot} alt='dot' className='doticon'/> TechHaven: Constant Updates. Discover More!</p>
+            <p><img src={dot} alt='dot' className='doticon'/> TechHaven: Your Reliable Tech Partner </p>
+            <p><img src={dot} alt='dot' className='doticon'/> Join the TechHaven Family! Your Satisfaction is Our Top Priority! </p>
+            <p><img src={dot} alt='dot' className='doticon'/> Experience Unmatched Quality with Every Product at TechHaven! </p>
             <p><img src={dot} alt='dot' className='doticon'/> TechHaven: Constant Updates. Discover More! <img src={dot} alt='dot' className='lastdot'/></p>
         </div>
     </div>
